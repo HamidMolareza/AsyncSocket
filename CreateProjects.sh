f789c1a1c572c3a669e0be2573117bb59579b4a9
Hamid Molareza <kitm.hamid@gmail.com>
2020-03-17 16:16:17 +0330

    Add CreateProjects.sh

---
+++ CreateProjects.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+#Save project name for easy use.
+projectName="AsynchronousClient"
+
+#Create sln.
+dotnet new sln
+
+#Create main classlib for the templates.
+dotnet new classlib -o $projectName
+dotnet sln add $projectName/$projectName.csproj
+
+#Create xunit for test the classlib.
+dotnet new xunit -o Test-$projectName
+dotnet sln add Test-$projectName/Test-$projectName.csproj
+#Add access reference.
+dotnet add Test-$projectName/Test-$projectName.csproj reference $projectName/$projectName.csproj
+
+#Create ConsoleApp for see results, manual testing and debugging.
+dotnet new console -o Console-$projectName
+dotnet sln add Console-$projectName/Console-$projectName.csproj
+#Add access reference.
+dotnet add Console-$projectName/Console-$projectName.csproj reference $projectName/$projectName.csproj
+
+#Build project to make sure everything is right.
+dotnet build
