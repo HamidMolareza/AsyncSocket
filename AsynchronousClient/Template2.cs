fadec2d05a55d026a69f2abc389a74c9fa023392
Hamid Molareza <kitm.hamid@gmail.com>
2020-03-17 16:24:30 +0330

    Add using and dotnet build succeeded.

--- AsynchronousClient/Template2.cs
+++ AsynchronousClient/Template2.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace AsynchronousClient {
     public static class Template2 {
         public static async Task StartClientAsync () {
