f694ad99fa15f86e9310574485fd14ee48bffbab
Hamid Molareza <kitm.hamid@gmail.com>
2020-03-21 17:08:56 +0430

    Add XML for client.cs

--- AsyncSocket/Client.cs
+++ AsyncSocket/Client.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace AsyncSocket {
     public class Client {
+
         private string _hostEntry;
+
+        /// <summary>
+        /// Host name or address
+        /// </summary>
         public string HostEntry {
             get => _hostEntry;
             set => SetHostEntryAndPort (value, Port);
@@ -21,10 +26,16 @@ namespace AsyncSocket {
             set => SetHostEntryAndPort (HostEntry, value);
         }
 
+        /// <summary>
+        /// An IPHostEntry instance that contains address information about the host specified in address.
+        /// </summary>
         public IPHostEntry IpHost { get; private set; }
 
         public IPAddress IpAddress { get; private set; }
 
+        /// <summary>
+        /// A network endpoint as an IP address and a port number.
+        /// </summary>
         public IPEndPoint RemoteEndPoint { get; private set; }
 
         public Socket ClientSocket { get; private set; }
@@ -48,7 +59,14 @@ namespace AsyncSocket {
             _port = port;
         }
 
-        public async Task SendAsync (string message, Encoding encoding, double receiveTimeout = 5000) {
+        /// <summary>
+        /// Send data to the remote device, then receive the response from the remote device.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="encoding">Message encoding</param>
+        /// <param name="receiveTimeout"></param>
+        /// <returns>Response from the remote device.</returns>
+        public async Task<string> SendAsync (string message, Encoding encoding, double receiveTimeout = 5000) {
             // Connect to the remote endpoint.  
             await BaseSocket.ConnectAsync (ClientSocket, RemoteEndPoint);
 
@@ -57,7 +75,7 @@ namespace AsyncSocket {
                 await BaseSocket.SendAsync (ClientSocket, message, encoding, SocketFlags.None);
 
                 // Receive the response from the remote device. 
-                await BaseSocket.ReceiveAsync (ClientSocket, encoding, receiveTimeout);
+                return await BaseSocket.ReceiveAsync (ClientSocket, encoding, receiveTimeout);
             } finally {
                 // Release the socket.  
                 ClientSocket.Shutdown (SocketShutdown.Both);
