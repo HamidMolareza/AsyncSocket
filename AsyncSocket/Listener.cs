c69e6f0fecfed1d8662d7cb7140d7f8c23fa179c
Hamid Molareza <kitm.hamid@gmail.com>
2020-03-22 05:00:18 +0430

    Properties validation, improve xml

--- AsyncSocket/Listener.cs
+++ AsyncSocket/Listener.cs
@@ -3,8 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-
-//TODO: Property validation
+using AsyncSocket.Exceptions;
 
 namespace AsyncSocket {
     public abstract class Listener {
@@ -20,6 +19,7 @@ namespace AsyncSocket {
         /// The port number for the remote device. (Http: 80, HTTPS: 443)
         /// To set property, The listener must be stop.
         /// </summary>
+        /// <exception cref="AsyncSocket.Exceptions.ListenerIsActiveException">Throw exception if listener is active.</exception>
         public int Port {
             get => _port;
             set => BindToLocalEndPoint (value);
@@ -32,28 +32,40 @@ namespace AsyncSocket {
 
         private int _numOfThreads;
         private const int DefaultNumOfThreads = 25;
+        public const int MinimumThreads = 1;
 
         /// <summary>
         /// To set property, The listener must be stop.
         /// </summary>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown when value is out of range.</exception>
+        /// <exception cref="AsyncSocket.Exceptions.ListenerIsActiveException">Throw exception if listener is active.</exception>
         public int NumOfThreads {
             get => _numOfThreads;
             set {
                 ListenerMustBeStop ();
+                if (value < MinimumThreads)
+                    throw new ArgumentOutOfRangeException ($"The value must equal or more than {MinimumThreads}.");
+
                 _numOfThreads = value;
             }
         }
 
         private double _receiveTimeout;
         private const double DefaultReceiveTimeout = 5000;
+        public const double MinimumReceiveTimeout = 1;
 
         /// <summary>
         /// To set property, The listener must be stop.
         /// </summary>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown when value is out of range.</exception>
+        /// <exception cref="AsyncSocket.Exceptions.ListenerIsActiveException">Throw exception if listener is active.</exception>
         public double ReceiveTimeout {
             get => _receiveTimeout;
             set {
                 ListenerMustBeStop ();
+                if (value < MinimumReceiveTimeout)
+                    throw new ArgumentOutOfRangeException ($"The value must equal or more than {MinimumReceiveTimeout}.");
+
                 _receiveTimeout = value;
             }
         }
@@ -74,7 +86,7 @@ namespace AsyncSocket {
         /// <summary>
         /// TCP/IP socket
         /// </summary>
-        public readonly Socket ListenerSocket = new Socket(IpAddress.AddressFamily,
+        public readonly Socket ListenerSocket = new Socket (IpAddress.AddressFamily,
             SocketType.Stream, ProtocolType.Tcp);
 
         protected Listener (int port = DefaultPort, int numOfThreads = DefaultNumOfThreads, double receiveTimeout = DefaultReceiveTimeout) {
@@ -86,12 +98,16 @@ namespace AsyncSocket {
         /// <summary>
         /// Throw exception if listener is active.
         /// </summary>
-        /// <exception cref="System.Exception">Throw exception if listener is active.</exception>
+        /// <exception cref="AsyncSocket.Exceptions.ListenerIsActiveException">Throw exception if listener is active.</exception>
         private void ListenerMustBeStop () {
             if (IsListenerActive)
-                throw new Exception ("The listener is active. Please stop the listener first.");
+                throw new ListenerIsActiveException ("The listener is active. Please stop the listener first.");
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="port"></param>
+        /// <exception cref="AsyncSocket.Exceptions.ListenerIsActiveException">Throw exception if listener is active.</exception>
         private void BindToLocalEndPoint (int port) {
             ListenerMustBeStop ();
 
@@ -107,7 +123,7 @@ namespace AsyncSocket {
         }
 
         /// <summary>
-        /// Start the listener/
+        /// Start the listener.
         /// </summary>
         public void Start () {
             if (IsListenerActive)
@@ -118,7 +134,7 @@ namespace AsyncSocket {
                 Task.Run (StartListeningAsync);
 
             //Delay to ensure all threads is run. 
-            Task.Delay(NumOfThreads).Wait();
+            Task.Delay (NumOfThreads).Wait ();
         }
 
         /// <summary>
@@ -163,6 +179,9 @@ namespace AsyncSocket {
             }
         }
 
+        /// <summary>
+        /// Stop the listener.
+        /// </summary>
         public async Task StopAsync () {
             if (!IsListenerActive)
                 return;
