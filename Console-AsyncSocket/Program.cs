b0bfeab151ab89c1f62517b32252167829eca58d
Hamid Molareza <kitm.hamid@gmail.com>
2020-04-03 21:31:06 +0430

    Update Program.cs for manual test.

--- Console-AsyncSocket/Program.cs
+++ Console-AsyncSocket/Program.cs
@@ -3,7 +3,20 @@
 namespace Console_AsyncSocket {
     class Program {
         static void Main (string[] args) {
-            Console.WriteLine ("Hello World!");
+            using (var myListener = new MyListener (numOfThreads: 25)) {
+                System.Console.WriteLine ($"{MyListener.IpAddress}:{myListener.Port}");
+
+                System.Console.Write ("Starting...");
+                myListener.Start ();
+                System.Console.WriteLine ("\nStart.");
+
+                System.Console.Write ("Stopping...");
+                myListener.Stop ();
+                System.Console.WriteLine ("\nStop.");
+            }
+
+            System.Console.Write ("Press any key to exit...");
+            Console.ReadKey ();
         }
     }
 }
